87ac443a6f7125af091413f6c65fc9867c6e5e1a
riyaaasharma <123953415+riyaaasharma@users.noreply.github.com>
2023-07-18 01:17:23 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,26 @@
+const taskInput = document.getElementById('task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');
 
+addTaskBtn.addEventListener('click', addTask);
+
+taskList.addEventListener('click', markAsCompleted);
+
+function addTask() {
+  const task = taskInput.value;
+  if (task !== '') {
+    const listItem = document.createElement('li');
+    listItem.innerText = task;
+    taskList.appendChild(listItem);
+    taskInput.value = '';
+  }
+}
+
+function markAsCompleted(event) {
+  const listItem = event.target;
+  if (!listItem.classList.contains('completed')) {
+    listItem.classList.add('completed');
+  } else {
+    listItem.classList.remove('completed');
+  }
+}
